28a963ba6358ff0dcf56741462e40488f85cac31
lorenzo-demo <49476347+lorenzo-demo@users.noreply.github.com>
2019-06-20 16:57:42 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,12 +2,12 @@ resource "random_id" "toto" {
   keepers = {
     uuid = "${uuid()}"
   }
-  byte_length = var.toto_length
+  byte_length = "${var.toto_length}"
 }
 
 resource "random_id" "titi" {
   keepers = {
     uuid = "${uuid()}"
   }
-  byte_length = var.titi_length
+  byte_length = "${var.titi_length}"
 }
