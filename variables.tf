075aea58fd0ec64c9e4f0857ce83747553395749
Nicolas EHRMAN <nicolas.ehrman@gmail.com>
2020-09-18 14:23:51 +0200

    Change value of length for random

--- variables.tf
+++ variables.tf
@@ -1,11 +1,11 @@
 variable "toto_length" {
     description = "longueur de toto"
     type = number
-    default = 8
+    default = 16
 }
 
 variable "titi_length" {
     description = "longueur de titi"
     type = number
-    default = 16
+    default = 32
 }
